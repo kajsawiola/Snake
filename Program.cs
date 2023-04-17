c866def1910ddd2e2ed5b73a3dc85a41cb1123a6
Christian Karlsson <battousai877@gmail.com>
2023-04-17 20:55:03 +0200

    added difficulty and speed choices after play is choosen at mainmenu.

--- Program.cs
+++ Program.cs
@@ -17,12 +17,15 @@
             {
                 PrintMainMenu();
 
-                command = GetUserInput();
+                command = GetUserInput("Menu choice: ");
 
                 if (command == "play" || command == "1")
                 {
+                    int speed = ChooseGameSpeed();
+                    int difficulty = ChooseGameDifficulty();
+
                     //NYI
-                    //PlayGame();
+                    //PlayGame(speed, difficulty);
                     Console.WriteLine("Play");
                 }
                 else if (command == "highscore" || command == "2")
@@ -50,13 +53,91 @@
             Console.WriteLine("3.Quit\n");
         }
 
-        private static string GetUserInput()
+        private static string GetUserInput(string prompt)
         {
-            Console.Write("Command:>");
+            Console.Write(prompt);
             string command = Console.ReadLine();
             return command;
         }
 
+        private static int ChooseGameSpeed() 
+        {
+            int speed = -1;
+            string speedChoice;
+            bool isValidChoice = false;
+            
+            Console.WriteLine("\nChoose game speed:");
+            Console.WriteLine("1.Slow");
+            Console.WriteLine("2.Normal");
+            Console.WriteLine("3.Fast\n");
 
+            while (!isValidChoice)
+            {
+                speedChoice = GetUserInput("Choice: ");
+
+                if (speedChoice == "slow" || speedChoice == "1")
+                {
+                    speed = 1;
+                    isValidChoice = true;
+                    Console.WriteLine("Slow");
+                }
+                else if (speedChoice == "normal" || speedChoice == "2")
+                {
+                    speed = 2;
+                    isValidChoice = true;
+                    Console.WriteLine("Normal");
+                }
+                else if (speedChoice == "fast" || speedChoice == "3")
+                {
+                    speed = 3;
+                    Console.WriteLine("Fast");
+                    isValidChoice = true;
+                }
+                else
+                    Console.WriteLine("Not a valid choice!");
+
+            }
+            return speed;
+        }
+
+        private static int ChooseGameDifficulty()
+        {
+            int difficulty = -1;
+            string speedChoice;
+            bool isValidChoice = false;
+
+            Console.WriteLine("\nChoose difficulty:");
+            Console.WriteLine("1.Easy");
+            Console.WriteLine("2.Medium");
+            Console.WriteLine("3.Hard\n");
+
+            while (!isValidChoice)
+            {
+                speedChoice = GetUserInput("Choice: ");
+
+                if (speedChoice == "easy" || speedChoice == "1")
+                {
+                    difficulty = 1;
+                    isValidChoice = true;
+                    Console.WriteLine("Easy");
+                }
+                else if (speedChoice == "medium" || speedChoice == "2")
+                {
+                    difficulty = 2;
+                    isValidChoice = true;
+                    Console.WriteLine("Medium");
+                }
+                else if (speedChoice == "hard" || speedChoice == "3")
+                {
+                    difficulty = 3;
+                    Console.WriteLine("Hard");
+                    isValidChoice = true;
+                }
+                else
+                    Console.WriteLine("Not a valid choice!");
+            
+            }
+            return difficulty;
+        }
     }
 }
