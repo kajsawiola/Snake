f3aaece6815b34d6a0a95733de534f24ac96b2ff
Christian Karlsson <battousai877@gmail.com>
2023-04-17 15:41:36 +0200

    added a MainMenu()

--- Program.cs
+++ Program.cs
@@ -4,9 +4,59 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hej världen");
-            Console.WriteLine("God kväll");
+            Console.WriteLine("Welcome to Snake!");
+            MainMenu();
         }
+
+        private static void MainMenu()
+        {
+            bool continueMenu = true;
+            string commandArr;
+
+            while (continueMenu)
+            {
+                PrintMainMenu();
+
+                commandArr = GetUserInput();
+
+                if (commandArr == "play" || commandArr == "1")
+                {
+                    //NYI
+                    //PlayGame();
+                    Console.WriteLine("Play");
+                }
+                else if (commandArr == "highscore" || commandArr == "2")
+                {
+                    //NYI
+                    //PrintHighscore();
+                    Console.WriteLine("Highscore");
+                }
+                else if (commandArr == "quit" || commandArr == "3")
+                {
+                    Console.WriteLine("Have a nice day!");
+                    continueMenu = false;
+                }
+                else
+                    Console.WriteLine("Unknown command!");
+
+            }
+        }
+
+        private static void PrintMainMenu()
+        {
+            Console.WriteLine("\nMain Menu");
+            Console.WriteLine("1.Play");
+            Console.WriteLine("2.Highscore");
+            Console.WriteLine("3.Quit\n");
+        }
+
+        private static string GetUserInput()
+        {
+            Console.Write("Command:>");
+            string commandArr = Console.ReadLine();
+            return commandArr;
+        }
+
+
     }
 }
